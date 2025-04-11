685de4d5c52e43f0128a0b06907543737fd79c98
AnthoR100 <anthony.hersant@outlook.fr>
2025-04-11 02:24:38 +0100

    fix: utilisation du fichier items.json généré par analyzeImages.js

--- functions/api.js
+++ functions/api.js
@@ -1,43 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.handler = async function(event, context) {
   try {
-    const items = {
-      "2065": {
-        "name": "Chant de guerre de Shurelya",
-        "gold_total": 2200,
-        "image": "https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/2065.png",
-        "color": ["#FCDC12"]
-      },
-      "3074": {
-        "name": "Hydre vorace",
-        "gold_total": 3300,
-        "image": "https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/3074.png",
-        "color": ["#ff0000"]
-      },
-      "3071": {
-        "name": "Couperet noir",
-        "gold_total": 3000,
-        "image": "https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/3071.png",
-        "color": ["#ff00ff"]
-      },
-      "6676": {
-        "name": "Créateur de failles",
-        "gold_total": 3200,
-        "image": "https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/6676.png",
-        "color": ["#ff00ff", "#ff8000"]
-      },
-      "3040": {
-        "name": "Étreinte du séraphin",
-        "gold_total": 2900,
-        "image": "https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/3040.png",
-        "color": ["#0000ff"]
-      },
-      "3011": {
-        "name": "Bénédiction de Mikael",
-        "gold_total": 2300,
-        "image": "https://ddragon.leagueoflegends.com/cdn/15.7.1/img/item/3011.png",
-        "color": ["#00ff00"]
-      }
-    };
+    // Lire le fichier JSON
+    const itemsPath = path.join(__dirname, 'data', 'items.json');
+    const rawData = await fs.promises.readFile(itemsPath, 'utf8');
+    const items = JSON.parse(rawData);
 
     // Transformer les données
     const formattedItems = Object.entries(items).map(([id, item]) => ({
@@ -45,7 +14,7 @@ exports.handler = async function(event, context) {
       name: item.name,
       image: item.image,
       description: item.description || "",
-      colors: item.color || [],
+      colors: item.colors || [],
       stats: {
         price: item.gold_total
       }
